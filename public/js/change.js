08a45afd057af6457904c1f2615294264b37c8c4
Izharulhaq <izharhaq00@gmail.com>
2021-02-09 20:26:38 +0700

    Template for change and save & load

---
+++ public/js/change.js
@@ -0,0 +1,32 @@
+// This .js file is used to define every function
+// that will be used by popup window with id
+// change-popup
+
+// TODO
+function scale() {
+
+}
+
+// TODO
+function changeColor() {
+
+}
+
+// TODO
+function changeGeometry() {
+    var scaleInput = document.getElementById('scale-input');
+    var colorInput = document.getElementById('color-input');
+
+    var scaleValue = scaleInput.value;
+    var colorValue = colorInput.value;
+
+    if (scaleValue != "") {
+        scale();
+    }
+    
+    if (colorValue != "") {
+        changeColor();
+    }
+
+    hidePopUpWindow('change');
+}
---
+++ public/js/saveload.js
@@ -0,0 +1,12 @@
+// This .js file is used to define every function
+// that will be used save and load file
+
+// TODO
+function save() {
+
+}
+
+// TODO
+function load() {
+    
+}
