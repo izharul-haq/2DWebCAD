5e9034e17558817ab3e9eb92adc20a27e9a763bd
Izharulhaq <izharhaq00@gmail.com>
2021-02-12 06:50:02 +0700

    Add save feature

--- public/js/saveload.js
+++ public/js/saveload.js
@@ -1,9 +1,21 @@
 // This .js file is used to define every function
 // that will be used save and load file
 
-// TODO
 function save() {
+    var fileName = "geometry.json";
+    var fileContent = {
+        "type": geo.type,
+        "coorData": geo.coorData,
+        "indexData": geo.indexData,
+        "colorData": geo.colorData
+    };
+    var fileBlob = new Blob([JSON.stringify(fileContent)], {type:'application/json'});
 
+    window.URL = window.URL;
+
+    var link = document.getElementById("download");
+    link.setAttribute('href', window.URL.createObjectURL(fileBlob));
+    link.setAttribute('download', fileName);
 }
 
 // TODO
