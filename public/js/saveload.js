0c1fa2bf6c78849cba6e7f195d4db78bcf6fc530
zaimsyaddad99 <zaimibnusyam@gmail.com>
2021-02-24 21:31:18 +0700

    add load

--- public/js/saveload.js
+++ public/js/saveload.js
@@ -1,6 +1,8 @@
 // This .js file is used to define every function
 // that will be used save and load file
 
+import { drawGeo } from "./gl";
+
 window.save = function save() {
     var fileName = "geometry.json";
     var fileContent = {
@@ -22,7 +24,24 @@ window.save = function save() {
 function readFile(input) {
     // follow tutorial in this link:
     // https://javascript.info/file
+    let file = input.files[0];
+    let reader = new FileReader();
 
+    reader.readAsText(file);
+
+    reader.onload = function() {
+        var mydata = JSON.parse(file);
+        var canvas = document.querySelector('canvas');
+        geo.type = mydata[0].type;
+        geo.coorData = mydata[0].coorData;
+        geo.indexData = mydata[0].indexData;
+        geo.colorData = mydata[0].colorData;
+        drawGeo(canvas, geo.type, geo.coorData, geo.indexData, geo.colorData);
+    };
+
+    reader.onerror = function() {
+        alert("Cannot load the file");
+    };
     /* 
     Steps:
         1. Read file as text
